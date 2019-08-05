0b41140c6221926fa4ee56fb4a769d3d3925564b
nao1117 <prgmi.910@gmail.com>
2019-08-05 21:02:39 +0900

    toppage-fixed

--- app/assets/javascripts/top-slide.js
+++ app/assets/javascripts/top-slide.js
@@ -3,8 +3,8 @@ $(function() {
       prevArrow:'<i class="fas fa-chevron-left"></i>',
       nextArrow:'<i class="fas fa-chevron-right"></i>',
       dots: true,
-      // autoplay: true,
-      // autoplaySpeed: 4000,
+      autoplay: true,
+      autoplaySpeed: 4000,
       speed: 800 ,
   });
 
