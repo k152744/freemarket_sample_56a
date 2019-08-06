870f97c5de2fb773a8e5f478169841f408d4a3d1
Keita0505 <kanakei0505@yahoo.co.jp>
2019-08-06 14:19:51 +0900

    delete-console

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,6 +1,5 @@
 $(function() {
   $('.mypage__contents__title').on('click', function() {
-    console.log("fire")
     $('.mypage__contents__title').toggleClass('mypage__contents__title--close');
     $('.mypage__announces').toggleClass('mypage--display-none');
     $('.mypage__todos').toggleClass('mypage--display-none');
@@ -8,7 +7,6 @@ $(function() {
 });
 $(function() {
   $('.mypage__purchased__title').on('click', function() {
-    console.log("fire")
     $('.mypage__purchased__title').toggleClass('mypage__purchased__title--close');
     $('.mypage__purchased__now').toggleClass('mypage--display-none');
     $('.mypage__purchased__past').toggleClass('mypage--display-none');
