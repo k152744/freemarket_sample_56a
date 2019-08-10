326c94ff59208720d880c19547531150bc8a31ba
Keita0505 <kanakei0505@yahoo.co.jp>
2019-08-10 13:02:09 +0900

    commit-gem-aws-s3

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,23 @@
 require "shrine"
 require "shrine/storage/file_system"
+require 'shrine/storage/s3'
 
-# アップロードするディレクトリの指定
+if Rails.env.production?
+s3_options = {
+    access_key_id:     Rails.application.credentials.aws[:access_key_id],
+    secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region:            'ap-northeast-1',
+    bucket:            'freemarket-56a'}
+
 Shrine.storages = {
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
-    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store")}
+    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)}
+else
+# この部分は元々記述していた
+Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+    store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store')}
+end
 
 # 使用するプラグインの宣言
 Shrine.plugin :activerecord
