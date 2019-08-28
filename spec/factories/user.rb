3c5ca03578fc34b00973f5e20562275b3ec2bf55
nao1117 <prgmi.910@gmail.com>
2019-08-28 15:42:08 +0900

    user-controller-test

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -11,3 +11,34 @@ FactoryBot.define do
     birthday              {"00000000"}
   end
 end
+
+FactoryBot.define do
+  factory :use_request do
+    nickname  {"abe"}
+    email {"kkk@gmail.com"}
+    
+
+    trait :invalid do
+      nickname  {nil}
+    end
+  end
+
+  factory :takashi, class: User do
+    id {"1"}
+    nickname {"Takashi"}
+    email   {"takashi@example.com"}
+    password              {"00000000"}
+    # password_confirmation {"00000000"}
+    family_name_kanji     {"abe"}
+    family_name_kana      {"abe"}
+    first_name_kanji      {"abe"}
+    first_name_kana       {"abe"}
+    birthday              {00000000}
+    
+  end
+
+  # factory :satoshi, class: User do
+  #   name "Satoshi"
+  #   email "satoshi@example.com"
+  # end
+end
